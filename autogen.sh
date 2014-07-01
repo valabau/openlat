1017547ef95b035e5b74586f27c15e4777f719d0
Vicent Alabau <valabau@gmail.com>
2014-07-01 14:45:12 +0200

    Create m4 directory if it does not exist

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,7 @@
 
 echo "Generating configure files... may take a while."
 
+mkdir -p m4
 autoreconf --install --force -I m4 -v && \
   echo "Preparing was successful if there was no error messages above." && \
   echo "Now type:" && \
